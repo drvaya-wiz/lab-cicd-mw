8de8fe30ff1a2b4d9d1b56a6b831bd96c183e080
Dharam <drvaya@users.noreply.github.com>
2020-02-26 11:46:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     up: `${(Date.now() - dateUp)/1000} seg.`,
     repo: 'https://github.com/drvaya/hello-cloud-run',
     url: 'https://about.me/dharmeshvaya',
-    desc: 'Built with Cloud Run for Webinar Demo - GDG Cloud Mumbai (YouTube Live)'
+    desc: 'Built with Cloud Run'
   });
 });
 
