3b7ba6bbeecb0136cfb4558139c1a6386bf8b1e5
Dharam <drvaya@users.noreply.github.com>
2020-11-03 19:33:13 +0530

    UPdated for Demo

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     up: `${(Date.now() - dateUp)/1000} seg.`,
     repo: 'https://github.com/drvaya-learn/hello-cloud-run',
     url: 'https://about.me/dharmeshvaya',
-    desc: 'Built with Cloud Run - Demo Slack Webhook, thanks for attending !!!'
+    desc: 'Built with Cloud Run - Demo for GitHub Virtual Meetup, thanks for attending !!!'
   });
 });
 
