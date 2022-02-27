f1fa668164e06621fa70823636ecece30c8d6553
Dharam <drvaya@users.noreply.github.com>
2022-02-27 14:56:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const router = express.Router();
+
 const dateUp = Date.now();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/', (req, res) => {
+  const today = new Date();
+
+  res.json({
+    date: today,
+    up: `${(Date.now() - dateUp)/1000} seg.`,
+    repo: 'https://github.com/drvaya-learn/hello-cloud-run',
+    url: 'https://about.me/dharmeshvaya',
+    desc: 'Built with Cloud Run - Demo for #GDE-Talks#, thanks for attending : 27-Feb-2022 !!!'
+  });
 });
 
-app.use('/', router);
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
   console.log('Press CTRL + C to quit');
