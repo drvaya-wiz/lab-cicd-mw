dfb05d21b4e8a6440fb567bb9b0dc89df77ac7c1
Dharam <drvaya@users.noreply.github.com>
2022-02-27 14:48:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const router = express.Router();
 const dateUp = Date.now();
 
-app.get('/', (req, res) => {
-  const today = new Date();
-  res.sendFile(path.join(__dirname+'/index.html'));  
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.use('/', router);
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
   console.log('Press CTRL + C to quit');
